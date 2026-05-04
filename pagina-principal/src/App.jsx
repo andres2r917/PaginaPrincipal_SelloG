51aa0af71b0ca1c5aec1922aaafbe82d278f4fef
andres2r917 <dav9095551@gmail.com>
2026-05-04 11:58:45 -0500

    parte1

---
+++ pagina-principal/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import Header from './Componentes/Header.jsx'
+import Footer from './Componentes/Footer.jsx'
+import Navbar from './Componentes/Navbar.jsx'
+import Seccion1 from './Componentes/Seccion1.jsx'
+import Seccion2 from './Componentes/Seccion2.jsx' 
+const App = () => {
+  return (
+    <div>      
+      <Header />
+      <Navbar/>
+    <div>
+      <Seccion1 />
+      <Seccion2 />
+    </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ pagina-principal/src/Componentes/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../Style.css/Header.css'
+import logo from '../assets/logo.svg'
+const Header = () => {
+  return (
+     <header> 
+          <nav className="nav-bar">
+            <div className="nav-links">
+                <div>
+                  <img className="logo" src={logo} alt="Logo"/>
+                </div>
+              <div className="items">
+                <Link to="/home" className="nav-item">Inicio</Link>
+                <Link to="/adopta" className="nav-item">Adopta</Link>
+                <Link to="/denuncia" className="nav-item">Denuncia</Link>
+              </div>
+            </div>
+
+            <div className="search-container">
+              <div className="search-input-wrapper">
+                <input type="text" placeholder="¿Qué buscas?" />
+                <button className="search-button">🔍</button>
+              </div>
+            </div>
+
+            <div className="auth-buttons">
+              <Link to="/login" className="login-btn">Iniciar sesión</Link>
+              <Link to="/registro" className="register-btn">Registrarse</Link>
+            </div>
+          </nav>
+      </header>
+  )
+}
+
+export default Header
